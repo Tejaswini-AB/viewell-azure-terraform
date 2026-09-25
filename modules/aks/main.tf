4d921c61ddf6ac5e6a86bec7d7e7e3dca796804b
tejaswini.a <tejaswini.a@adfolks.com>
2026-09-25 13:40:28 +0400

    commit

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_kubernetes_cluster" "this" {
 }
 
 resource "azurerm_role_assignment" "acr_pull" {
-  count                = var.acr_id != null ? 1 : 0
+ # count                = var.acr_id != null ? 1 : 0
   scope                = var.acr_id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_kubernetes_cluster.this.kubelet_identity[0].object_id
